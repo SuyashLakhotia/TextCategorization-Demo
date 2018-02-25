0c0d548e56dd885160d71ec0223c0839b4bd0ed3
Suyash Lakhotia <suyashlakhotia@gmail.com>
2018-02-25 23:28:18 +0800

    v0 of App

---
+++ server.py
@@ -0,0 +1,57 @@
+from flask import Flask, make_response, jsonify, request, render_template
+
+
+MODEL_IDS = ["all", "LinearSVC", "MultinomialNB", "mlp", "cnn_fchollet", "cnn_ykim", "gcnn_chebyshev",
+             "gcnn_spline", "gcnn_fourier"]
+CLASS_NAMES = ["POSITIVE", "NEGATIVE"]
+
+
+app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Resource not found!"}), 404)
+
+
+@app.route('/')
+def root():
+    return render_template("index.html")
+
+
+@app.route("/classify", methods=["POST"])
+def categorize_text():
+    req = request.get_json()
+    text = req.get("text")
+    model_id = req.get("model_id")
+
+    res = []
+    if model_id == "all":
+        for i in range(1, len(MODEL_IDS)):
+            res.append(_json_record(MODEL_IDS[i], "POSITIVE", 90.23))
+    elif model_id == "LinearSVC":
+        res.append(_json_record("LinearSVC", "POSITIVE", 90.23))
+    elif model_id == "MultinomialNB":
+        res.append(_json_record("MultinomialNB", "POSITIVE", 90.23))
+    elif model_id == "mlp":
+        res.append(_json_record("mlp", "POSITIVE", 90.23))
+    elif model_id == "cnn_fchollet":
+        res.append(_json_record("cnn_fchollet", "POSITIVE", 90.23))
+    elif model_id == "cnn_ykim":
+        res.append(_json_record("cnn_ykim", "POSITIVE", 90.23))
+    elif model_id == "gcnn_chebyshev":
+        res.append(_json_record("gcnn_chebyshev", "POSITIVE", 90.23))
+    elif model_id == "gcnn_spline":
+        res.append(_json_record("gcnn_spline", "POSITIVE", 90.23))
+    elif model_id == "gcnn_fourier":
+        res.append(_json_record("gcnn_fourier", "POSITIVE", 90.23))
+
+    return jsonify(res)
+
+
+def _json_record(model_id, class_name, confidence):
+    return {"model_id": model_id, "class_name": class_name, "confidence": "{:g}".format(confidence)}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/main.js
@@ -0,0 +1,46 @@
+const API_BASE_URL = 'http://127.0.0.1:5000';
+
+const MODEL_IDS = ["all", "LinearSVC", "MultinomialNB", "mlp", "cnn_fchollet", "cnn_ykim", "gcnn_chebyshev",
+                   "gcnn_spline", "gcnn_fourier"]
+
+function submit() {
+    clearResults();
+
+    let text = $('#text').val();
+    let modelID = MODEL_IDS[$('#model-select').prop('selectedIndex')];
+    let request = {
+        model_id: modelID,
+        text: text
+    };
+
+    $.ajax({
+        async: true,
+        crossDomain: true,
+        url: API_BASE_URL + "/classify",
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+        },
+        processData: false,
+        data: JSON.stringify(request)
+    }).then((data) => {
+        showResults(data);
+    });
+}
+
+function showResults(data) {
+    let $results = $('#results');
+    for (const datum of data) {
+        let $result = $('#result-template').clone().removeAttr('id');
+        $result.html('<strong>' + datum['model_id'] + ':</strong> ' + datum['class_name'] + ' (' +
+                     datum['confidence'] + '%)');
+        $results.append($result);
+    }
+    $('#results-section').show();
+}
+
+function clearResults() {
+    $('#results .result').remove();
+    $('#results-section').hide();
+}
