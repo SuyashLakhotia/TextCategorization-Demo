9f186cdf851b33f61ee1e2cf319eefd43d8e9454
Suyash Lakhotia <suyashlakhotia@gmail.com>
2018-03-20 18:14:08 +0800

    Modified server.py for RCV1 Models

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 from flask import Flask, make_response, jsonify, request, render_template
 
-
-MODEL_IDS = ["all", "linear_svc", "multinomial_nb", "mlp", "cnn_fchollet", "cnn_ykim", "gcnn_chebyshev",
-             "gcnn_spline", "gcnn_fourier"]
-CLASS_NAMES = ["POSITIVE", "NEGATIVE"]
-
+import preprocess
+import models
+import rcv1_constants as RCV1
 
 app = Flask(__name__)
 
@@ -20,37 +18,28 @@ def root():
 
 
 @app.route("/classify", methods=["POST"])
-def categorize_text():
+def classify_text():
     req = request.get_json()
     text = req.get("text")
     model_id = req.get("model_id")
 
-    res = []
-    if model_id == "all":
-        for i in range(1, len(MODEL_IDS)):
-            res.append(_json_record(MODEL_IDS[i], "POSITIVE", 90.23))
-    elif model_id == "linear_svc":
-        res.append(_json_record("linear_svc", "POSITIVE", 90.23))
-    elif model_id == "multinomial_nb":
-        res.append(_json_record("multinomial_nb", "POSITIVE", 90.23))
-    elif model_id == "mlp":
-        res.append(_json_record("mlp", "POSITIVE", 90.23))
-    elif model_id == "cnn_fchollet":
-        res.append(_json_record("cnn_fchollet", "POSITIVE", 90.23))
-    elif model_id == "cnn_ykim":
-        res.append(_json_record("cnn_ykim", "POSITIVE", 90.23))
-    elif model_id == "gcnn_chebyshev":
-        res.append(_json_record("gcnn_chebyshev", "POSITIVE", 90.23))
-    elif model_id == "gcnn_spline":
-        res.append(_json_record("gcnn_spline", "POSITIVE", 90.23))
-    elif model_id == "gcnn_fourier":
-        res.append(_json_record("gcnn_fourier", "POSITIVE", 90.23))
+    data_tfidf, data_word2ind = preprocess.preprocess(text, RCV1.VOCABULARY)
+    results = models.run_models(model_id, data_tfidf, data_word2ind)
 
-    return jsonify(res)
+    _res = []
+    for model_id, result in results.items():
+        _res.append(_json_record(model_id, RCV1.CLASS_NAMES[RCV1.CLASSES[result[0]]], result[1]))
+
+    return jsonify(_res)
 
 
 def _json_record(model_id, class_name, confidence):
-    return {"model_id": model_id, "class_name": class_name, "confidence": "{:g}".format(confidence)}
+    if confidence != "N/A":
+        record = {"model_id": model_id, "class_name": class_name, "confidence": "{:.2f}".format(confidence)}
+    else:
+        record = {"model_id": model_id, "class_name": class_name}
+
+    return record
 
 
 if __name__ == "__main__":
